3dc4f79e0909546920e0114b908534733d8b895d
pltnw <k.pltnw@gmail.com>
2022-11-02 18:15:30 +0100

    Initial Commit

---
+++ AnimationApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AnimationApp
+//
+//  Created by Екатерина Платонова on 02.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
